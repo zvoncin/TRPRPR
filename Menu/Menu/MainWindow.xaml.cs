098c75799298fd84b6b97fca0395d54728ca9374
Sergey <melky0507@gmail.com>
2018-09-19 22:08:19 +0300

    first commit

---
+++ Menu/Menu/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Menu
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            //////
+            // Создание привязки
+            CommandBinding bind = new CommandBinding(ApplicationCommands.New);
+
+            // Присоединение обработчика событий
+            bind.Executed += NewCommand_Executed;
+
+            // Регистрация привязки
+            this.CommandBindings.Add(bind);
+            //////
+
+            KeyGesture CloseCmdKeyGesture = new KeyGesture(
+    Key.L, ModifierKeys.Alt);
+
+            KeyBinding CloseKeyBinding = new KeyBinding(
+                ApplicationCommands.Close, CloseCmdKeyGesture);
+
+            this.InputBindings.Add(CloseKeyBinding);
+        }
+
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Команда запущена из " + e.Source.ToString());
+        }
+
+
+        private void Button_Click_Exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ Сalculator_1/Сalculator_1/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Сalculator_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Получаем текст кнопки
+            string s = (string)((Button)e.OriginalSource).Content;
+            // Добавляем его в текстовое поле
+            textBlock.Text += s;
+        }
+
+        private void Button_Click_Otvet(object sender, RoutedEventArgs e)
+        {
+            string s = textBlock.Text;
+            if (s.Length == 0)
+            { }
+            else
+            {
+                string[] split = s.Split(new Char[] { '+', '-', '/', '*' });
+                if (split.Length == 1)
+                { }
+                else
+                {
+                    double Fin = Convert.ToDouble(split[0]);
+                    double Tin = Convert.ToDouble(split[1]);
+                    int temp = s.IndexOf("+", 0);
+                    if (temp != -1)
+                        textBlock_Otvet.Text = Convert.ToString(Fin + Tin);
+                    temp = s.IndexOf("/", 0);
+                    if (temp != -1)
+                        textBlock_Otvet.Text = Convert.ToString(Fin / Tin);
+                    temp = s.IndexOf("*", 0);
+                    if (temp != -1)
+                        textBlock_Otvet.Text = Convert.ToString(Fin * Tin);
+                    temp = s.IndexOf("-", 0);
+                    if (temp != -1)
+                        textBlock_Otvet.Text = Convert.ToString(Fin - Tin);
+                }
+            }
+
+        }
+
+        private void Button_Click_Clear(object sender, RoutedEventArgs e)
+        {
+            string s = (string)((Button)e.OriginalSource).Content;
+            if (s == "Clear")
+            {
+                textBlock.Text = "";
+                textBlock_Otvet.Text = "";
+            }
+            else if (s == "BSpace")
+            {
+                s = textBlock.Text;
+                if (s.Length == 0)
+                { }
+                else
+                {
+                    s = s.Substring(0, s.Length - 1);
+                    textBlock.Text = s;
+                }
+            }
+                
+        }
+    }
+}
